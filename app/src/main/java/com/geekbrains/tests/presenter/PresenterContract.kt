3164c18b0a2d1a8b86439c0490a4a7611528d558
bembik08 <yabz1986@mail.ru>
2021-12-06 19:59:21 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrains/tests/presenter/PresenterContract.kt
@@ -0,0 +1,8 @@
+package com.geekbrains.tests.presenter
+
+import com.geekbrains.tests.view.ViewContract
+
+internal interface PresenterContract {
+   fun onAttach(view : ViewContract)
+   fun onDetach()
+}
---
+++ app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
@@ -0,0 +1,34 @@
+package com.geekbrains.tests.presenter.details
+
+import com.geekbrains.tests.view.ViewContract
+import com.geekbrains.tests.view.details.ViewDetailsContract
+
+internal class DetailsPresenter internal constructor(
+    private val viewContract: ViewDetailsContract,
+    private var count: Int = 0
+) : PresenterDetailsContract {
+    private var view: ViewContract? = null
+    override fun setCounter(count: Int) {
+        this.count = count
+    }
+
+    override fun onIncrement() {
+        count++
+        viewContract.setCount(count)
+    }
+
+    override fun onDecrement() {
+        count--
+        viewContract.setCount(count)
+    }
+
+    override fun onAttach(view: ViewContract) {
+        if (this.view == null && this.view != view) {
+            this.view = view
+        }
+    }
+
+    override fun onDetach() {
+        view = null
+    }
+}
